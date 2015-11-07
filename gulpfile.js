dd3c8cbb5bc8ad1736fa9aa595cd8f39e4f4d323
var-bin <rashan_soft@inbox.ru>
2015-11-07 23:41:25 +0200

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,16 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
     path = require('path');
 
 gulp.task('less', function () {
   gulp.src('./components/styles.less')
     .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: true
+    }))
     .pipe(gulp.dest('./stylesheets/'));
 });
 
