26076dcf804ff60580e445f56e401a976e42d151
var-bin <rashan_soft@inbox.ru>
2015-11-07 18:50:49 +0200

    edit 'less' task. Add 'watch' task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@
 
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    watch = require('gulp-watch'),
     path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
-  .pipe(less({
-    paths: [ path.join(__dirname, 'less', 'includes') ]
-  }))
-  .pipe(gulp.dest('./public/css'));
+  gulp.src('./components/styles.less')
+    .pipe(less())
+    .pipe(gulp.dest('./stylesheets/'));
+});
+
+gulp.task('watch', function () {
+  watch('./components/**/*.less', function () {
+    gulp.start('less');
+  });
 });
